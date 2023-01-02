10dc9708c824799c3d4f2618f2a9cdeac828c3de
luis3518 <luis.131189@gmail.com>
2023-01-02 18:17:39 -0300

    fix description and experiencecss

--- components/description.js
+++ components/description.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
+
 
 
 function Description() {
   return (
-<div>
+
 <div className="descriptionContainer">
+<div className="contentContainer">
         <h1>Hola! soy Luis Rodriguez</h1>
         <h4>
         Soy un apasionado de todo lo relacionado con el mundo de la informática. Actualmente trabajo como desarrollador.
 Abierto a nuevos desafíos y aprendizajes de nuevas tecnologías.
         </h4>
+        <div className="socialContainer">
+          <a href="https://github.com/Luis3518/" target="_blank">
+            <FaGithub size={70} className="icon"/>
+          </a>
+          <a href="https://www.linkedin.com/in/luis-rodriguez3518/">
+            <FaLinkedin size={70} className="icon"/>
+          </a>
+          <a href="https://twitter.com/Lu1sR0driguez">
+            <FaTwitter size={70} className="icon"/>
+          </a>
         </div>
-
+        </div>
         </div>
   );
 }
